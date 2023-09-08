e308e8409b8d8488d5d98f77e977bdeb07fc5c82
BhavyadhariniRamidi <bdramidi@gmail.com>
2023-09-07 21:42:22 -0700

    Moved Cart Service to Services Folder

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,7 +1,7 @@
 
 
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
